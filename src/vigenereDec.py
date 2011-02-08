ee53a70cdc55e9562e9e889ddc6c245d57d666b2
Josh Keyoth <jkeyoth@mines.edu>
2011-02-07 22:14:29 -0700

    vigenereDec now finds lengths between repetitions

--- src/vigenereDec.py
+++ src/vigenereDec.py
@@ -10,30 +10,73 @@ cipherFile = open(sys.argv[1])
 
 cipher = ""
 letterCount=0
+halfway = 0
+#lengths between repetitions
+lengths = []
 
 #functions
-def findIfRepeated(str):
-	"""Take a string str and return the distance between repeations in cipher"""
-	#TODO: Implement
-	pass
+def buildStringSplice(start, le):
+	"""Return a string from cipher of length le starting at start. The string will contain l characters,
+	but is not a normal slice, because it skips over anything that isn't a letter."""
+	cnt = 0
+	cur = 0
+	ret = ""
+	while cnt < le:
+		if ord(cipher[start+cur]) >= ord("a") and ord(cipher[start+cur]) <= ord("z"):
+			ret += cipher[start+cur]
+			cnt += 1
+		cur += 1
+	return ret
+		
+
+def findRepeatedLength(str, start):
+	"""Take a string and the first spot the string was seen, and returns the distance between
+	repetitions in cipher. Returns [] if no matches are found. Otherwise, the return is an
+	array of lengths"""
+	if start + len(str) >= len(cipher):
+		return []
+	curSpot = start + len(str)
+	while curSpot + len(str) <= len(cipher):
+		if curSpot > start and cipher[curSpot:curSpot+len(str)] == str:
+			return [curSpot - start] + findRepeatedLength(str, curSpot)
+		curSpot += 1
+	return []
+	
 
 def findRepeats(size):
 	"""Look for repeats with repeated string of size size"""
-	curSpot = 0;
-	
-def Decrypt(keySize):
-	"""Decrypt the cipher text, assuming key is of length keySize"""
-	#TODO: Implement
+	global lengths, cipher
+	curSpot = 0
+	while curSpot < len(cipher):
+		lengths = lengths + findRepeatedLength(cipher[curSpot:curSpot+size], curSpot)
+		
+		curSpot += 1
 
+def findKeyLength():
+	"""Find the most likely key length from the array of lengths"""
+	#TODO: Implement
+	pass
+
+def Decrypt(keySize):
+	""" Decrypt the cipher text, assuming key is of length keySize"""
+	#TODO: Implement
+	pass
 ###main###
 
 #read cipherFile into cipher, a string
 for l in cipherFile:
-	cipher += l.strip()
+	cipher += l.strip().lower()
 
 #count number of encrypted letters in cipher. Ignore any non-letters.
 for c in cipher:
-	if ord(c.lower()) >= ord("a") and ord(c.lower()) <= ord("z"):
+	if ord(c) >= ord("a") and ord(c) <= ord("z"):
 		letterCount += 1
+		
+halfway = letterCount / 2
 
-print letterCount
+size = halfway
+#while size > 2:
+#	findRepeats(size)
+#	size -= 1
+
+print findRepeatedLength("as", 0)
